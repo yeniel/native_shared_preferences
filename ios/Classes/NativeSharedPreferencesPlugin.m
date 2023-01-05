129149a47d9c1d2aebcb4c9f67f5272970347b67
Matej Zidek <zidek.matt@gmail.com>
2023-01-05 21:11:20 +0100

    trying out changed bundle id of shared pref on ios

--- ios/Classes/NativeSharedPreferencesPlugin.m
+++ ios/Classes/NativeSharedPreferencesPlugin.m
@@ -14,7 +14,7 @@
     } else if ([method isEqualToString:@"setBool"]) {
       NSString *key = arguments[@"key"];
       NSNumber *value = arguments[@"value"];
-      [[NSUserDefaults standardUserDefaults] setBool:value.boolValue forKey:key];
+      [[[NSUserDefaults alloc] initWithSuiteName:@"com.dontpanicdevs.DontPanic"] setBool:value.boolValue forKey:key];
       result(@YES);
     } else if ([method isEqualToString:@"setInt"]) {
       NSString *key = arguments[@"key"];
@@ -22,32 +22,32 @@
       // int type in Dart can come to native side in a variety of forms
       // It is best to store it as is and send it back when needed.
       // Platform channel will handle the conversion.
-      [[NSUserDefaults standardUserDefaults] setValue:value forKey:key];
+      [[[NSUserDefaults alloc] initWithSuiteName:@"com.dontpanicdevs.DontPanic"] setValue:value forKey:key];
       result(@YES);
     } else if ([method isEqualToString:@"setDouble"]) {
       NSString *key = arguments[@"key"];
       NSNumber *value = arguments[@"value"];
-      [[NSUserDefaults standardUserDefaults] setDouble:value.doubleValue forKey:key];
+      [[[NSUserDefaults alloc] initWithSuiteName:@"com.dontpanicdevs.DontPanic"] setDouble:value.doubleValue forKey:key];
       result(@YES);
     } else if ([method isEqualToString:@"setString"]) {
       NSString *key = arguments[@"key"];
       NSString *value = arguments[@"value"];
-      [[NSUserDefaults standardUserDefaults] setValue:value forKey:key];
+      [[[NSUserDefaults alloc] initWithSuiteName:@"com.dontpanicdevs.DontPanic"] setValue:value forKey:key];
       result(@YES);
     } else if ([method isEqualToString:@"setStringList"]) {
       NSString *key = arguments[@"key"];
       NSArray *value = arguments[@"value"];
-      [[NSUserDefaults standardUserDefaults] setValue:value forKey:key];
+      [[[NSUserDefaults alloc] initWithSuiteName:@"com.dontpanicdevs.DontPanic"] setValue:value forKey:key];
       result(@YES);
     } else if ([method isEqualToString:@"commit"]) {
       // synchronize is deprecated.
       // "this method is unnecessary and shouldn't be used."
       result(@YES);
     } else if ([method isEqualToString:@"remove"]) {
-      [[NSUserDefaults standardUserDefaults] removeObjectForKey:arguments[@"key"]];
+      [[[NSUserDefaults alloc] initWithSuiteName:@"com.dontpanicdevs.DontPanic"] removeObjectForKey:arguments[@"key"]];
       result(@YES);
     } else if ([method isEqualToString:@"clear"]) {
-      NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+      NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:@"com.dontpanicdevs.DontPanic"];
       for (NSString *key in [self getAllPrefs]) {
         [defaults removeObjectForKey:key];
       }
@@ -62,7 +62,7 @@
 
 + (NSMutableDictionary *)getAllPrefs {
   NSString *appDomain = [[NSBundle mainBundle] bundleIdentifier];
-  NSDictionary *prefs = [[NSUserDefaults standardUserDefaults] persistentDomainForName:appDomain];
+  NSDictionary *prefs = [[[NSUserDefaults alloc] initWithSuiteName:@"com.dontpanicdevs.DontPanic"] dictionaryRepresentation];
   NSMutableDictionary *filteredPrefs = [NSMutableDictionary dictionary];
   if (prefs != nil) {
     NSMutableDictionary *mappedDictionary = [NSMutableDictionary dictionary];
